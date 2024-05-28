a4796f9565a22fff69d493af6022b3739183c3c8
guilhermelim <guilhermem.lima@outlook.com>
2024-05-28 20:53:08 -0300

    edit image in home page

--- src/sections/home/home-hero.tsx
+++ src/sections/home/home-hero.tsx
@@ -19,7 +19,6 @@ import { bgBlur, bgGradient, textGradient } from 'src/theme/css';
 
 import Iconify from 'src/components/iconify';
 import { varFade, MotionContainer } from 'src/components/animate';
-
 // ----------------------------------------------------------------------
 
 const StyledRoot = styled('div')(({ theme }) => ({
@@ -129,13 +128,17 @@ export default function HomeHero() {
 
   const theme = useTheme();
 
+  const PRIMARY_MAIN = theme.palette.primary.main;
+
+  const PRIMARY_DARK = theme.palette.primary.dark;
+
   const heroRef = useRef<HTMLDivElement | null>(null);
 
   const { scrollY } = useScroll();
 
   const [percent, setPercent] = useState(0);
 
-  const lightMode = theme.palette.mode === 'light';
+  // const lightMode = theme.palette.mode === 'light';
 
   const getScroll = useCallback(() => {
     let heroHeight = 0;
@@ -155,12 +158,12 @@ export default function HomeHero() {
     getScroll();
   }, [getScroll]);
 
-  const transition = {
-    repeatType: 'loop',
-    ease: 'linear',
-    duration: 60 * 4,
-    repeat: Infinity,
-  } as const;
+  // const transition = {
+  //   repeatType: 'loop',
+  //   ease: 'linear',
+  //   duration: 60 * 4,
+  //   repeat: Infinity,
+  // } as const;
 
   const opacity = 1 - percent / 100;
 
@@ -328,86 +331,86 @@ export default function HomeHero() {
     </Box>
   );
 
-  const renderSlides = (
-    <Stack
-      direction="row"
-      alignItems="flex-start"
-      sx={{
-        height: '150%',
-        position: 'absolute',
-        opacity: opacity > 0 ? opacity : 0,
-        transform: `skew(${-16 - percent / 24}deg, ${4 - percent / 16}deg)`,
-        ...(theme.direction === 'rtl' && {
-          transform: `skew(${16 + percent / 24}deg, ${4 + percent / 16}deg)`,
-        }),
-      }}
-    >
-      <Stack
-        component={m.div}
-        variants={varFade().in}
-        sx={{
-          width: 344,
-          position: 'relative',
-        }}
-      >
-        <Box
-          component={m.img}
-          animate={{ y: ['0%', '100%'] }}
-          transition={transition}
-          alt={lightMode ? 'light_1' : 'dark_1'}
-          src={
-            lightMode
-              ? `/assets/images/home/hero/light_1.webp`
-              : `/assets/images/home/hero/dark_1.webp`
-          }
-          sx={{ position: 'absolute', mt: -5 }}
-        />
-        <Box
-          component={m.img}
-          animate={{ y: ['-100%', '0%'] }}
-          transition={transition}
-          alt={lightMode ? 'light_1' : 'dark_1'}
-          src={
-            lightMode
-              ? `/assets/images/home/hero/light_1.webp`
-              : `/assets/images/home/hero/dark_1.webp`
-          }
-          sx={{ position: 'absolute' }}
-        />
-      </Stack>
+  // const renderSlides = (
+  //   <Stack
+  //     direction="row"
+  //     alignItems="flex-start"
+  //     sx={{
+  //       height: '150%',
+  //       position: 'absolute',
+  //       opacity: opacity > 0 ? opacity : 0,
+  //       transform: `skew(${-16 - percent / 24}deg, ${4 - percent / 16}deg)`,
+  //       ...(theme.direction === 'rtl' && {
+  //         transform: `skew(${16 + percent / 24}deg, ${4 + percent / 16}deg)`,
+  //       }),
+  //     }}
+  //   >
+  //     <Stack
+  //       component={m.div}
+  //       variants={varFade().in}
+  //       sx={{
+  //         width: 344,
+  //         position: 'relative',
+  //       }}
+  //     >
+  //       <Box
+  //         component={m.img}
+  //         animate={{ y: ['0%', '100%'] }}
+  //         transition={transition}
+  //         alt={lightMode ? 'light_1' : 'dark_1'}
+  //         src={
+  //           lightMode
+  //             ? `/assets/images/home/hero/light_1.webp`
+  //             : `/assets/images/home/hero/dark_1.webp`
+  //         }
+  //         sx={{ position: 'absolute', mt: -5 }}
+  //       />
+  //       <Box
+  //         component={m.img}
+  //         animate={{ y: ['-100%', '0%'] }}
+  //         transition={transition}
+  //         alt={lightMode ? 'light_1' : 'dark_1'}
+  //         src={
+  //           lightMode
+  //             ? `/assets/images/home/hero/light_1.webp`
+  //             : `/assets/images/home/hero/dark_1.webp`
+  //         }
+  //         sx={{ position: 'absolute' }}
+  //       />
+  //     </Stack>
 
-      <Stack
-        component={m.div}
-        variants={varFade().in}
-        sx={{ width: 720, position: 'relative', ml: -5 }}
-      >
-        <Box
-          component={m.img}
-          animate={{ y: ['100%', '0%'] }}
-          transition={transition}
-          alt={lightMode ? 'light_2' : 'dark_2'}
-          src={
-            lightMode
-              ? `/assets/images/home/hero/light_2.webp`
-              : `/assets/images/home/hero/dark_2.webp`
-          }
-          sx={{ position: 'absolute', mt: -5 }}
-        />
-        <Box
-          component={m.img}
-          animate={{ y: ['0%', '-100%'] }}
-          transition={transition}
-          alt={lightMode ? 'light_2' : 'dark_2'}
-          src={
-            lightMode
-              ? `/assets/images/home/hero/light_2.webp`
-              : `/assets/images/home/hero/dark_2.webp`
-          }
-          sx={{ position: 'absolute' }}
-        />
-      </Stack>
-    </Stack>
-  );
+  //     <Stack
+  //       component={m.div}
+  //       variants={varFade().in}
+  //       sx={{ width: 720, position: 'relative', ml: -5 }}
+  //     >
+  //       <Box
+  //         component={m.img}
+  //         animate={{ y: ['100%', '0%'] }}
+  //         transition={transition}
+  //         alt={lightMode ? 'light_2' : 'dark_2'}
+  //         src={
+  //           lightMode
+  //             ? `/assets/images/home/hero/light_2.webp`
+  //             : `/assets/images/home/hero/dark_2.webp`
+  //         }
+  //         sx={{ position: 'absolute', mt: -5 }}
+  //       />
+  //       <Box
+  //         component={m.img}
+  //         animate={{ y: ['0%', '-100%'] }}
+  //         transition={transition}
+  //         alt={lightMode ? 'light_2' : 'dark_2'}
+  //         src={
+  //           lightMode
+  //             ? `/assets/images/home/hero/light_2.webp`
+  //             : `/assets/images/home/hero/dark_2.webp`
+  //         }
+  //         sx={{ position: 'absolute' }}
+  //       />
+  //     </Stack>
+  //   </Stack>
+  // );
 
   const renderPolygons = (
     <>
@@ -442,7 +445,52 @@ export default function HomeHero() {
                 {renderDescription}
               </Grid>
 
-              {mdUp && <Grid md={6}>{renderSlides}</Grid>}
+              <Grid md={6}>
+                <Stack
+                  direction="row"
+                  justifyContent="center"
+                  alignItems="center"
+                  spacing={2}
+                  height="80%"
+                >
+                  <Box
+                    component="div"
+                    sx={{
+                      width: '100%',
+                      height: '100%',
+                    }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="100%"
+                      height="100%"
+                      viewBox="0 0 1000 1000"
+                    >
+                      <defs>
+                        <linearGradient id="P" gradientUnits="userSpaceOnUse" />
+                        <linearGradient
+                          id="g1"
+                          x2="1"
+                          href="#P"
+                          gradientTransform="matrix(-1116.535,467.737,-586.684,-1400.475,1232.527,682.213)"
+                        >
+                          <stop offset="0%" stopColor={PRIMARY_MAIN} />
+                          <stop offset="100%" stopColor={PRIMARY_DARK} />
+                        </linearGradient>
+                      </defs>
+                      <path
+                        d="m-0.2 127.1h427.5c315 0 572.7 257.8 572.7 572.8v172.8h-427.4c-315 0-572.8-257.7-572.8-572.7z"
+                        fill="url(#g1)"
+                      />
+                      <path
+                        d="m631.7 741.6v-143.5l189.4 141.3v132.3l-2.3 1h-246.2c-140.6 0-269.7-51.4-369.7-136.2-31-60.4-46.8-135.8-46.8-226.2 0-232.1 117.3-351.5 314.3-381.5 135.2 10.2 257.7 67.8 350.7 156.2v62.6l-37.2-16.1c-62-26.8-127.7-38.9-195.1-38.9-151.1 0-238 60.6-238 217.7 0 154.1 64.6 238 223.1 238 19.2 0 38.7-2.6 57.8-6.7z"
+                        fill="#fff"
+                      />
+                    </svg>
+                  </Box>
+                </Stack>
+              </Grid>
+              {/* {mdUp && <Grid md={6}>{renderSlides}</Grid>} */}
             </Grid>
           </Container>
 
